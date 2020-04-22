abdac5b89bd1f31094b8842fcac646f6fe7877dc
David Jacoby <jacoby934@gmail.com>
2020-04-22 18:30:12 -0400

    Implemented image url save to db

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -31,46 +31,46 @@ cloudinary.config({
 // @route    POST api/profile/upload
 // @desc     upload image
 // @access   Private
-router.post('/upload', auth, upload.single('image'), async (req, res) => {
+router.post('/upload', auth, upload.single('image'), (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
   const profileFields = {};
   profileFields.user = req.user.id;
-  cloudinary.v2.uploader.upload(req.file.path, (err, result) => {
+  cloudinary.v2.uploader.upload(req.file.path, async (err, result) => {
     if (err) {
       req.json(err.message);
     }
-    console.log(result.secure_url);
+
     profileFields.avatar = result.secure_url;
-  });
-  console.log(profileFields);
-  try {
-    let profile = await Profile.findOne({
-      user: req.user.id,
-    });
-    console.log(profileFields);
-    if (profile) {
-      // If profile exists, update profile
-      profile = await Profile.findOneAndUpdate(
-        { user: req.user.id },
-        { $set: profileFields },
-        { new: true }
-      );
-      console.log(profile);
-      return res.json(profile);
+
+    try {
+      let profile = await Profile.findOne({
+        user: req.user.id,
+      });
+
+      if (profile) {
+        // If profile exists, update profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create new profile if one is not found
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
     }
-
-    // Create new profile if one is not found
-    profile = new Profile(profileFields);
-
-    await profile.save();
-    res.json(profile);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server Error');
-  }
+  });
 });
 
 // ----------------------- END IMAGE UPLOAD ----------------------- //
